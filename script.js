9cdac9bdb74aa40dfa364047be658659d1eaa3b1
najafathima1210-netizen <najafathima1210@gmail.com>
2025-11-19 10:26:11 +0530

    Initial commit: Add script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const resultInput = document.getElementById('result');
+
+function appendNumber(number) {
+    resultInput.value += number;
+}
+
+function appendOperator(operator) {
+    if (resultInput.value === '') return;
+    resultInput.value += operator;
+}
+
+function clearDisplay() {
+    resultInput.value = '';
+}
+
+function deleteLast() {
+    resultInput.value = resultInput.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        resultInput.value = eval(resultInput.value);
+    } catch (error) {
+        resultInput.value = 'Error';
+    }
+}
